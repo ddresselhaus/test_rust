7395704a57dde5ae670740b6ca6b6efada28e2f3
Daniel Dresselhaus <ddresselhaus@gmail.com>
2015-06-05 22:23:45 -0400

    fizzbuzz

---
+++ src/fizzbuzz.rs
@@ -0,0 +1,24 @@
+
+
+fn main() {
+   
+    let mut fizz: Vec<String> = vec![];
+
+    for i in (1..100) {
+        let x: String = i.to_string();
+        if (i % 5 == 0) & (i % 3 == 0) {
+            fizz.push("fizzbuzz".to_string());
+        } else if i % 5 == 0 {
+            fizz.push("buzz".to_string());
+        } else if i % 3 == 0 {
+            fizz.push("fizz".to_string());
+        } else {
+            fizz.push(x.to_string());
+        }
+    }
+
+    for i in fizz {
+        println!("{}", i);
+    }
+
+}
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+
+
+fn main() {
+
+    for i in (1..100) {
+        if (i % 5 == 0) & (i % 3 == 0) {
+            println!("fizzbuzz");
+        } else if i % 5 == 0 {
+            println!("buzz");
+        } else if i % 3 == 0 {
+            println!("fizz");
+        } else {
+            println!("{}", i);
+        }
+    }
+}
